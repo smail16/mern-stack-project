8a7cb89b471a82dfa720bfb78d6b35164a50cc6f
Ismail Fourati <119300453+smail16@users.noreply.github.com>
2023-04-05 05:34:03 +0200

    signup

--- src/components/modalSignup/config/index.js
+++ src/components/modalSignup/config/index.js
@@ -1,6 +1,10 @@
 import * as z from 'zod'
 
 export const schema = z.object({
+  fullName: z.string(),
   email: z.string().email(),
   password: z.string().min(1),
+  phone: z.string(),
+  address: z.string(),
+
 })
